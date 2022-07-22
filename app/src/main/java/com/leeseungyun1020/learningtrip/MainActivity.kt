b326008e9ae765dc1e4fb14b8135fa8ab9db400d
Lee Seung Yun <ileilliat@gmail.com>
2022-07-22 16:20:43 +0900

    Add top app bar

--- app/src/main/java/com/leeseungyun1020/learningtrip/MainActivity.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/MainActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.leeseungyun1020.learningtrip.ui.theme.LearningTripTheme
 
@@ -22,22 +21,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                }
+            )
+        },
+        content = { innerPadding ->
+            innerPadding
+//            LazyColumn(
+//                contentPadding = innerPadding,
+//                verticalArrangement = Arrangement.spacedBy(8.dp)
+//            ) {
+//                val list = (0..75).map { it.toString() }
+//                items(count = list.size) {
+//                    Text(
+//                        text = list[it],
+//                        style = MaterialTheme.typography.bodyLarge,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(horizontal = 16.dp)
+//                    )
+//                }
+//            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearningTripTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
