aea2ba38a43a59b1411e94e58fb92097f7e2e3c0
Lee Seung Yun <ileilliat@gmail.com>
2022-08-04 17:35:50 +0900

    Update Learning Trip Scaffold

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/common/LearningTripScaffold.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/common/LearningTripScaffold.kt
@@ -1,28 +1,33 @@
 package com.leeseungyun1020.learningtrip.ui.common
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.leeseungyun1020.learningtrip.R
+import com.leeseungyun1020.learningtrip.ui.theme.LearningTripTheme
 import com.leeseungyun1020.learningtrip.ui.theme.Primary
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LearningTripScaffold(
     title: String,
+    setDisplayHomeAsUpEnabled: Boolean = false,
+    setBodyContentInnerPadding: Boolean = true,
+    onHomeAsUpClicked: () -> Unit = {},
     topBarExtraContent: @Composable () -> Unit,
     bodyContent: @Composable () -> Unit,
 ) {
@@ -34,30 +39,96 @@ fun LearningTripScaffold(
                         color = Primary,
                         shape = RoundedCornerShape(bottomEnd = 10.dp, bottomStart = 10.dp)
                     )
-                    .heightIn(min = 56.dp),
             ) {
-                Text(
-                    text = title,
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    fontSize = 16.sp,
-                    color = Color.White
-                )
+                Box(modifier = Modifier.height(56.dp)) {
+                    if (setDisplayHomeAsUpEnabled)
+                        IconButton(
+                            onClick = onHomeAsUpClicked,
+                            modifier = Modifier.align(Alignment.CenterStart)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = stringResource(
+                                    id = R.string.action_back
+                                ),
+                                tint = Color.White
+                            )
+                        }
+                    Text(
+                        text = title,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                }
+
                 topBarExtraContent()
             }
         },
         content = { innerPadding ->
+            val modifier =
+                if (setBodyContentInnerPadding)
+                    Modifier.padding(top = innerPadding.calculateTopPadding())
+                else Modifier
             Column(
-                modifier = Modifier
-                    .padding(top = innerPadding.calculateTopPadding())
-                    .verticalScroll(
-                        rememberScrollState()
-                    )
+                modifier =
+                modifier.verticalScroll(
+                    rememberScrollState()
+                )
             ) {
                 bodyContent()
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LearningTripScaffoldPreview() {
+    LearningTripTheme {
+        LearningTripScaffold(
+            title = "Title",
+            setDisplayHomeAsUpEnabled = true,
+            setBodyContentInnerPadding = false,
+            topBarExtraContent = {
+                Text(
+                    text = "Extra Content",
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            }, bodyContent = {
+                Column {
+                    Text(
+                        text = "Body Content1",
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Body Content2",
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Body Content3",
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Body Content4",
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp
+                    )
+                }
+
+            })
+    }
 }
