192511ac44d0fd77b37f77bb8a12a370f8d0cdea
Lee Seung Yun <ileilliat@gmail.com>
2022-10-03 16:05:32 +0900

    Modify PlaceRepository network logic

--- app/src/main/java/com/leeseungyun1020/learningtrip/data/PlaceRepository.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/data/PlaceRepository.kt
@@ -5,12 +5,10 @@ import com.leeseungyun1020.learningtrip.model.Place
 import com.leeseungyun1020.learningtrip.model.SimpleHeritage
 import com.leeseungyun1020.learningtrip.model.SimplePlace
 import com.leeseungyun1020.learningtrip.network.RetrofitClient
+import com.leeseungyun1020.learningtrip.network.loadNetworkData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class PlaceRepository(private val placeDao: PlaceDao) {
     val allPlaces: Flow<List<Place>> = placeDao.getAll()
@@ -24,97 +22,37 @@ class PlaceRepository(private val placeDao: PlaceDao) {
 
     suspend fun recommend() {
         withContext(Dispatchers.IO) {
-            val dbData = placeDao.recommendedPlace()
-            RetrofitClient.homeService.getRecommendPlace()
-                .enqueue(object : Callback<List<SimplePlace>> {
-                    override fun onFailure(call: Call<List<SimplePlace>>, t: Throwable) {
-                        recommendedPlaces.postValue(dbData)
-                    }
-
-                    override fun onResponse(
-                        call: Call<List<SimplePlace>>,
-                        response: Response<List<SimplePlace>>
-                    ) {
-                        if (response.isSuccessful && response.code() == 200) {
-                            val body = response.body()
-                            if (body != null) {
-                                recommendedPlaces.postValue(body)
-                            }
-                        }
-                    }
-                })
+            RetrofitClient.homeService.getRecommendPlace().loadNetworkData(
+                target = recommendedPlaces,
+                fallback = placeDao.recommendedPlace()
+            )
         }
     }
 
     suspend fun searchById(id: Int) {
         withContext(Dispatchers.IO) {
-            val dbData = placeDao.getById(id)
-            RetrofitClient.placeService.getPlace(id)
-                .enqueue(object : Callback<Place> {
-                    override fun onFailure(call: Call<Place>, t: Throwable) {
-                        searchedPlace.postValue(dbData)
-                    }
-
-                    override fun onResponse(
-                        call: Call<Place>,
-                        response: Response<Place>
-                    ) {
-                        if (response.isSuccessful && response.code() == 200) {
-                            val body = response.body()
-                            if (body != null) {
-                                searchedPlace.postValue(body)
-                            }
-                        }
-                    }
-                })
+            RetrofitClient.placeService.getPlace(id).loadNetworkData(
+                target = searchedPlace,
+                fallback = placeDao.getById(id)
+            )
         }
     }
 
     suspend fun searchByKeyword(keyword: String) {
         withContext(Dispatchers.IO) {
-            val dbData = placeDao.findByKeyword(keyword)
-            RetrofitClient.searchService.getSearchedPlaceList(keyword)
-                .enqueue(object : Callback<List<SimplePlace>> {
-                    override fun onFailure(call: Call<List<SimplePlace>>, t: Throwable) {
-                        filteredPlaces.postValue(dbData)
-                    }
-
-                    override fun onResponse(
-                        call: Call<List<SimplePlace>>,
-                        response: Response<List<SimplePlace>>
-                    ) {
-                        if (response.isSuccessful && response.code() == 200) {
-                            val body = response.body()
-                            if (body != null) {
-                                filteredPlaces.postValue(body)
-                            }
-                        }
-                    }
-                })
+            RetrofitClient.searchService.getSearchedPlaceList(keyword).loadNetworkData(
+                target = filteredPlaces,
+                fallback = placeDao.findByKeyword(keyword)
+            )
         }
     }
 
     suspend fun searchNameByKeyword(keyword: String) {
         withContext(Dispatchers.IO) {
-            val dbData = placeDao.searchNameByKeyword(keyword)
-            RetrofitClient.searchService.getSearchedKeyword(keyword)
-                .enqueue(object : Callback<List<String>> {
-                    override fun onFailure(call: Call<List<String>>, t: Throwable) {
-                        filteredPlaceNames.postValue(dbData)
-                    }
-
-                    override fun onResponse(
-                        call: Call<List<String>>,
-                        response: Response<List<String>>
-                    ) {
-                        if (response.isSuccessful && response.code() == 200) {
-                            val body = response.body()
-                            if (body != null) {
-                                filteredPlaceNames.postValue(body)
-                            }
-                        }
-                    }
-                })
+            RetrofitClient.searchService.getSearchedKeyword(keyword).loadNetworkData(
+                target = filteredPlaceNames,
+                fallback = placeDao.searchNameByKeyword(keyword)
+            )
         }
     }
 
@@ -124,70 +62,25 @@ class PlaceRepository(private val placeDao: PlaceDao) {
 
     suspend fun loadRelatedHeritages(placeId: Int) {
         withContext(Dispatchers.IO) {
-            RetrofitClient.heritageService.getRelatedHeritage(placeId)
-                .enqueue(object : Callback<List<SimpleHeritage>> {
-                    override fun onFailure(call: Call<List<SimpleHeritage>>, t: Throwable) {
-
-                    }
-
-                    override fun onResponse(
-                        call: Call<List<SimpleHeritage>>,
-                        response: Response<List<SimpleHeritage>>
-                    ) {
-                        if (response.isSuccessful && response.code() == 200) {
-                            val body = response.body()
-                            if (body != null) {
-                                relatedHeritages.postValue(body)
-                            }
-                        }
-                    }
-                })
+            RetrofitClient.heritageService.getRelatedHeritage(placeId).loadNetworkData(
+                target = relatedHeritages
+            )
         }
     }
 
     suspend fun loadRelatedPlaces(placeId: Int) {
         withContext(Dispatchers.IO) {
-            RetrofitClient.placeService.getRelatedPlace(placeId)
-                .enqueue(object : Callback<List<SimplePlace>> {
-                    override fun onFailure(call: Call<List<SimplePlace>>, t: Throwable) {
-
-                    }
-
-                    override fun onResponse(
-                        call: Call<List<SimplePlace>>,
-                        response: Response<List<SimplePlace>>
-                    ) {
-                        if (response.isSuccessful && response.code() == 200) {
-                            val body = response.body()
-                            if (body != null) {
-                                relatedPlaces.postValue(body)
-                            }
-                        }
-                    }
-                })
+            RetrofitClient.placeService.getRelatedPlace(placeId).loadNetworkData(
+                target = relatedPlaces
+            )
         }
     }
 
     suspend fun loadNearbyPlaces(placeId: Int) {
         withContext(Dispatchers.IO) {
-            RetrofitClient.placeService.getNearbyPlace(placeId)
-                .enqueue(object : Callback<List<SimplePlace>> {
-                    override fun onFailure(call: Call<List<SimplePlace>>, t: Throwable) {
-
-                    }
-
-                    override fun onResponse(
-                        call: Call<List<SimplePlace>>,
-                        response: Response<List<SimplePlace>>
-                    ) {
-                        if (response.isSuccessful && response.code() == 200) {
-                            val body = response.body()
-                            if (body != null) {
-                                nearbyPlaces.postValue(body)
-                            }
-                        }
-                    }
-                })
+            RetrofitClient.placeService.getNearbyPlace(placeId).loadNetworkData(
+                target = nearbyPlaces
+            )
         }
     }
 }
