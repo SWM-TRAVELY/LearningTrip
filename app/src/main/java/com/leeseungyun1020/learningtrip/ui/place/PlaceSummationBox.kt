fdf01052f399cf5bec26fcaf8a2d3a180cab777d
Lee Seung Yun <ileilliat@gmail.com>
2022-10-06 14:46:15 +0900

    Modify PlaceSummationBox text maxLines to 1

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/place/PlaceSummationBox.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/place/PlaceSummationBox.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -68,7 +69,9 @@ fun PlaceSummationBox(
                 text = place.name ?: "",
                 fontSize = 20.sp,
                 fontFamily = notoSansKRFamily,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
             Row(modifier = Modifier.padding(top = 2.dp)) {
                 Icon(
