e7de6e3aad288b10037acd39582e847441d46537
Lee Seung Yun <ileilliat@gmail.com>
2022-10-13 18:37:39 +0900

    Add auth check logic

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/StoryScreen.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/StoryScreen.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,36 +22,49 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.leeseungyun1020.learningtrip.R
-import com.leeseungyun1020.learningtrip.model.Course
-import com.leeseungyun1020.learningtrip.model.SimpleCourse
+import com.leeseungyun1020.learningtrip.data.AuthRepository
 import com.leeseungyun1020.learningtrip.ui.Screen
 import com.leeseungyun1020.learningtrip.ui.common.CourseBox
 import com.leeseungyun1020.learningtrip.ui.common.LearningTripScaffold
+import com.leeseungyun1020.learningtrip.viewmodel.AuthViewModel
+import com.leeseungyun1020.learningtrip.viewmodel.AuthViewModelFactory
 import com.leeseungyun1020.learningtrip.viewmodel.CourseViewModel
 
 @Composable
 fun StoryScreen(
-    navController: NavController, viewModel: CourseViewModel = viewModel()
-) {
-    val courseList by viewModel.courseList.observeAsState()
-    LearningTripScaffold(
-        title = stringResource(id = R.string.nav_story),
-        floatingActionButton = {
-            FloatingActionButton(onClick = { navController.navigate("${Screen.AddCourse.root}/-1") }) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = stringResource(id = R.string.action_add)
-                )
-            }
-        }) {
+    navController: NavController, viewModel: CourseViewModel = viewModel(),
 
-        LazyColumn(modifier = Modifier.padding(top = 28.dp, start = 20.dp, end = 20.dp)) {
-            items(courseList ?: emptyList()) { course ->
-                CourseBox(course = course, modifier = Modifier
-                    .padding(bottom = 12.dp)
-                    .clickable {
-                        navController.navigate("${Screen.Course.root}/${course.id}")
-                    })
+    ) {
+    val authRepository = AuthRepository(LocalContext.current)
+    val authViewModel: AuthViewModel = viewModel(
+        factory = AuthViewModelFactory(authRepository)
+    )
+    //if (true) {
+    if (!authViewModel.isSingIn()) {
+        Button(onClick = { navController.navigate(Screen.SignIn.route) }) {
+            Text(text = "SIGN IN")
+        }
+    } else {
+        val courseList by viewModel.courseList.observeAsState()
+        LearningTripScaffold(
+            title = stringResource(id = R.string.nav_story),
+            floatingActionButton = {
+                FloatingActionButton(onClick = { navController.navigate("${Screen.AddCourse.root}/-1") }) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = stringResource(id = R.string.action_add)
+                    )
+                }
+            }) {
+
+            LazyColumn(modifier = Modifier.padding(top = 28.dp, start = 20.dp, end = 20.dp)) {
+                items(courseList ?: emptyList()) { course ->
+                    CourseBox(course = course, modifier = Modifier
+                        .padding(bottom = 12.dp)
+                        .clickable {
+                            navController.navigate("${Screen.Course.root}/${course.id}")
+                        })
+                }
             }
         }
     }
