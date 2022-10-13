b0e93c0027908a0c102b609cf674c19e7bbe8ca6
Lee Seung Yun <ileilliat@gmail.com>
2022-10-13 18:37:18 +0900

    Add AuthViewModelFactory

--- app/src/main/java/com/leeseungyun1020/learningtrip/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/viewmodel/AuthViewModel.kt
@@ -1,10 +1,21 @@
 package com.leeseungyun1020.learningtrip.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.leeseungyun1020.learningtrip.data.AuthRepository
 
 class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
 
     fun isSingIn() = authRepository.loadToken() != null
 
+}
+
+class AuthViewModelFactory(private val repository: AuthRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AuthViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AuthViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
