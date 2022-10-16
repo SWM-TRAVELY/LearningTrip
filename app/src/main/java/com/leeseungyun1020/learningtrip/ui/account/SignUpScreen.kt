1fd0565b0283791f68e088407deaa51e8f220f63
Lee Seung Yun <ileilliat@gmail.com>
2022-10-16 22:48:12 +0900

    Modify SignUp vertical scroll issue

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/account/SignUpScreen.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/account/SignUpScreen.kt
@@ -2,20 +2,21 @@ package com.leeseungyun1020.learningtrip.ui.account
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ToggleOff
-import androidx.compose.material.icons.filled.ToggleOn
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
@@ -23,25 +24,24 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.leeseungyun1020.learningtrip.R
-import com.leeseungyun1020.learningtrip.viewmodel.SignUpViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.input.VisualTransformation
 import com.leeseungyun1020.learningtrip.ui.common.SimpleTitleScaffold
+import com.leeseungyun1020.learningtrip.viewmodel.SignUpViewModel
 
 @Composable
 fun SignUpScreen(navController: NavController, viewModel: SignUpViewModel = viewModel()) {
     SimpleTitleScaffold(title = stringResource(id = R.string.app_name)) {
         var passwordVisible by rememberSaveable { mutableStateOf(false) }
         val focusManager = LocalFocusManager.current
-        Column {
+        Column(
+            Modifier.verticalScroll(rememberScrollState())
+        ) {
             Text(
                 modifier = Modifier.padding(top = 35.dp, start = 16.dp, end = 16.dp),
                 text = stringResource(id = R.string.title_signup),
