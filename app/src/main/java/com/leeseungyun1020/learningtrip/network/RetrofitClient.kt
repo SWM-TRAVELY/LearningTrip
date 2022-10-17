11d2710ba9bc3791b9a7355e64b6225267ec7826
Lee Seung Yun <ileilliat@gmail.com>
2022-10-17 16:59:06 +0900

    Separate url strings

--- app/src/main/java/com/leeseungyun1020/learningtrip/network/RetrofitClient.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/network/RetrofitClient.kt
@@ -4,8 +4,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://api.learningtrip.app/"
-
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
---
+++ app/src/main/java/com/leeseungyun1020/learningtrip/network/URL.kt
@@ -0,0 +1,6 @@
+package com.leeseungyun1020.learningtrip.network
+
+val BASE_URL = "https://api.learningtrip.app/"
+val BASE_HTTP_URL = "http://api.learningtrip.app/"
+val AUTH_KAKAO_URL = BASE_URL + "oauth2/authorization/kakao"
+val AUTH_NAVER_URL = BASE_URL + "oauth2/authorization/naver"
