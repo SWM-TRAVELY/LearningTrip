c31d4e4e395995ce7ef47643b6d955f55548173a
Lee Seung Yun <ileilliat@gmail.com>
2022-10-22 02:59:03 +0900

    Update AddPlaceScreen

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/AddPlaceScreen.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/AddPlaceScreen.kt
@@ -13,9 +13,11 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -95,15 +97,27 @@ fun AddPlaceScreen(
             )
         }
     ) {
-        PlaceListView(
-            modifier = Modifier.padding(top = 8.dp, start = 16.dp, end = 16.dp),
-            innerPadding = PaddingValues(top = 10.dp, start = 4.dp, end = 4.dp),
-            placeList = placeList ?: listOf(),
-            onPlaceClicked = {
-                val dayInt = day.toIntOrNull() ?: 0
-                val sequenceInt = sequence.toIntOrNull() ?: 0
-                addCourseViewModel.addPlace(it.toSimpleCoursePlace(dayInt, sequenceInt))
-                navController.popBackStack()
-            })
+        if (searchText.isNotEmpty())
+            PlaceListView(
+                modifier = Modifier.padding(top = 8.dp, start = 16.dp, end = 16.dp),
+                innerPadding = PaddingValues(top = 10.dp, start = 4.dp, end = 4.dp),
+                placeList = placeList ?: listOf(),
+                onPlaceClicked = {
+                    val dayInt = day.toIntOrNull() ?: 0
+                    val sequenceInt = sequence.toIntOrNull() ?: 0
+                    addCourseViewModel.addPlace(it.toSimpleCoursePlace(dayInt, sequenceInt))
+                    navController.popBackStack()
+                })
+        else
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.desc_add_place),
+
+                    textAlign = TextAlign.Center
+                )
+            }
     }
 }
