c82791ce39eed26cc25852ed221e16d8f9c42199
Lee Seung Yun <ileilliat@gmail.com>
2022-11-02 14:24:41 +0900

    Update database version

--- app/src/main/java/com/leeseungyun1020/learningtrip/data/AppDatabase.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/data/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.leeseungyun1020.learningtrip.model.Place
 
-@Database(entities = [Place::class], version = 1)
+@Database(entities = [Place::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun placeDao(): PlaceDao
 
