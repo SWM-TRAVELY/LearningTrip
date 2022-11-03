dfefa176816fa81e861cb53526524645187d51b8
Lee Seung Yun <ileilliat@gmail.com>
2022-11-03 14:24:06 +0900

    Add recommend course logic at StoryScreen

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/StoryScreen.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/StoryScreen.kt
@@ -1,9 +1,11 @@
 package com.leeseungyun1020.learningtrip.ui.course
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -15,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +27,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.leeseungyun1020.learningtrip.R
 import com.leeseungyun1020.learningtrip.data.AuthRepository
+import com.leeseungyun1020.learningtrip.model.SimpleCourse
 import com.leeseungyun1020.learningtrip.ui.Screen
 import com.leeseungyun1020.learningtrip.ui.account.SignInRequiredScreen
 import com.leeseungyun1020.learningtrip.ui.common.CourseBox
@@ -61,32 +65,44 @@ fun StoryScreen(
                     )
                 }
             }) {
-            if (courseList.isNullOrEmpty()) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        modifier = Modifier.padding(top = 8.dp),
-                        text = stringResource(id = R.string.desc_empty_story),
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
-            } else {
-                LazyColumn(
-                    modifier = Modifier.padding(start = 20.dp, end = 20.dp),
-                    contentPadding = PaddingValues(top = 28.dp)
-                ) {
-                    items(courseList ?: emptyList()) { course ->
+
+            val list: MutableList<Any> = courseList?.toMutableList() ?: mutableListOf()
+            list.add(0, 0)
+
+            LazyColumn(
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+                contentPadding = PaddingValues(top = 28.dp)
+            ) {
+                items(list) { course ->
+                    if (course is SimpleCourse) {
                         CourseBox(course = course, modifier = Modifier
                             .padding(bottom = 12.dp)
                             .clickable {
                                 navController.navigate("${Screen.Course.root}/${course.id}")
                             })
+                    } else {
+                        Box(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 12.dp)
+                                .height(100.dp)
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(MaterialTheme.colorScheme.tertiary)
+                                .clickable {
+                                    navController.navigate(Screen.CourseRequest.route)
+                                }
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.title_recommend_course),
+                                modifier = Modifier.align(Alignment.Center),
+                                style = MaterialTheme.typography.titleLarge,
+                                color = MaterialTheme.colorScheme.onTertiary
+                            )
+                        }
                     }
                 }
             }
+
         }
     }
 }
