639a5bdf1359ad7a314bd07356db64256d1ef778
Lee Seung Yun <ileilliat@gmail.com>
2022-11-06 23:45:48 +0900

    Add location and grade option at CourseRequestScreen

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/CourseRequestScreen.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/course/CourseRequestScreen.kt
@@ -1,12 +1,13 @@
 package com.leeseungyun1020.learningtrip.ui.course
 
 import android.icu.text.SimpleDateFormat
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -37,6 +38,7 @@ fun rememberDatePickerDialog(
 ): MaterialDatePicker<androidx.core.util.Pair<Long, Long>> {
     val dateRangePicker = remember {
         MaterialDatePicker.Builder.dateRangePicker()
+            .setTitleText("")
             .setSelection(
                 androidx.core.util.Pair(
                     startInMilliseconds,
@@ -66,7 +68,7 @@ fun CourseRequestScreen(
     navController: NavController,
     courseRequestViewModel: CourseRequestViewModel = viewModel()
 ) {
-    // 날짜
+    // 기간
     val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
     val start = courseRequestViewModel.start
     val end = courseRequestViewModel.end
@@ -78,30 +80,198 @@ fun CourseRequestScreen(
     ) { start, end ->
         courseRequestViewModel.onUpdateRange(start, end)
     }
-
-    Text(
-        text = stringResource(id = R.string.title_select_date),
-        style = MaterialTheme.typography.bodyLarge,
-        modifier = Modifier
-            .padding(top = 32.dp, start = 16.dp, end = 16.dp),
-    )
-    OutlinedTextField(
-        value = "${dateFormat.format(start)} ~ ${dateFormat.format(end)}",
-        onValueChange = { },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 4.dp),
-        readOnly = true,
-        trailingIcon = {
-            IconButton({ datePicker.show(fragmentManager, "DatePicker") }) {
-                Icon(Icons.Default.DateRange, contentDescription = null)
-            }
-        },
-    )
     // 지역
-
+    val options by courseRequestViewModel.options.observeAsState()
+    var locationExpanded by remember { mutableStateOf(false) }
+    var locationOptionExpanded by remember { mutableStateOf(false) }
     // 학년
-
+    var gradeExpanded by remember { mutableStateOf(false) }
+    var gradeOptionExpanded by remember { mutableStateOf(false) }
     // 키워드
 
+
+    if (options == null)
+        courseRequestViewModel.loadOptions()
+    Column(Modifier.padding(start = 16.dp, end = 16.dp)) {
+        // 기간
+        Text(
+            text = stringResource(id = R.string.title_select_date),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .padding(top = 32.dp),
+        )
+        OutlinedTextField(
+            value = "${dateFormat.format(start)} ~ ${dateFormat.format(end)}",
+            onValueChange = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp),
+            readOnly = true,
+            trailingIcon = {
+                IconButton({ datePicker.show(fragmentManager, "DatePicker") }) {
+                    Icon(Icons.Default.DateRange, contentDescription = null)
+                }
+            },
+        )
+        // 지역
+        Text(
+            text = stringResource(id = R.string.title_select_location),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .padding(top = 16.dp),
+        )
+
+        Row {
+            ExposedDropdownMenuBox(
+                expanded = locationExpanded,
+                onExpandedChange = { locationExpanded = !locationExpanded },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 2.dp),
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.menuAnchor(),
+                    readOnly = true,
+                    value = courseRequestViewModel.location,
+                    onValueChange = {},
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = locationExpanded) },
+                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
+                )
+                ExposedDropdownMenu(
+                    expanded = locationExpanded,
+                    onDismissRequest = { locationExpanded = false },
+                ) {
+                    options?.locations?.map { it.name }?.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            text = { Text(selectionOption) },
+                            onClick = {
+                                courseRequestViewModel.onUpdateLocation(selectionOption)
+                                locationExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+
+            ExposedDropdownMenuBox(
+                expanded = locationOptionExpanded,
+                onExpandedChange = { locationOptionExpanded = !locationOptionExpanded },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 2.dp),
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.menuAnchor(),
+                    readOnly = true,
+                    value = courseRequestViewModel.locationOption,
+                    onValueChange = {},
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = locationOptionExpanded) },
+                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
+                )
+                ExposedDropdownMenu(
+                    expanded = locationOptionExpanded,
+                    onDismissRequest = { locationOptionExpanded = false },
+                ) {
+                    options?.locations?.find { it.name == courseRequestViewModel.location }?.options?.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            text = { Text(selectionOption) },
+                            onClick = {
+                                courseRequestViewModel.onUpdateLocationOption(selectionOption)
+                                locationOptionExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+        }
+
+        // 학년
+        Text(
+            text = stringResource(id = R.string.title_select_grade),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .padding(top = 16.dp),
+        )
+        Row {
+            ExposedDropdownMenuBox(
+                expanded = gradeExpanded,
+                onExpandedChange = { gradeExpanded = !gradeExpanded },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 2.dp),
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.menuAnchor(),
+                    readOnly = true,
+                    value = courseRequestViewModel.grade,
+                    onValueChange = {},
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = gradeExpanded) },
+                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
+                )
+                ExposedDropdownMenu(
+                    expanded = gradeExpanded,
+                    onDismissRequest = { gradeExpanded = false },
+                ) {
+                    options?.grades?.map { it.name }?.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            text = { Text(selectionOption) },
+                            onClick = {
+                                courseRequestViewModel.onUpdateGrade(selectionOption)
+                                gradeExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+
+            ExposedDropdownMenuBox(
+                expanded = gradeOptionExpanded,
+                onExpandedChange = { gradeOptionExpanded = !gradeOptionExpanded },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 2.dp),
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.menuAnchor(),
+                    readOnly = true,
+                    value = courseRequestViewModel.gradeOption,
+                    onValueChange = {},
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = gradeOptionExpanded) },
+                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
+                )
+                ExposedDropdownMenu(
+                    expanded = gradeOptionExpanded,
+                    onDismissRequest = { gradeOptionExpanded = false },
+                ) {
+                    options?.grades?.find { it.name == courseRequestViewModel.grade }?.options?.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            text = { Text(selectionOption) },
+                            onClick = {
+                                courseRequestViewModel.onUpdateGradeOption(selectionOption)
+                                gradeOptionExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+        }
+        // 키워드
+
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp),
+            onClick = {
+
+            },
+            shape = RoundedCornerShape(10.dp),
+        ) {
+            Text(text = stringResource(id = R.string.action_recommend_course))
+        }
+    }
 }
--- app/src/main/java/com/leeseungyun1020/learningtrip/viewmodel/CourseRequestViewModel.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/viewmodel/CourseRequestViewModel.kt
@@ -3,8 +3,12 @@ package com.leeseungyun1020.learningtrip.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.leeseungyun1020.learningtrip.model.course.CourseOptionResponse
+import com.leeseungyun1020.learningtrip.model.course.GroupItem
 
 class CourseRequestViewModel : ViewModel() {
     private var _start by mutableStateOf(MaterialDatePicker.todayInUtcMilliseconds())
@@ -15,8 +19,68 @@ class CourseRequestViewModel : ViewModel() {
     val end: Long
         get() = _end
 
+    private var _location by mutableStateOf("")
+    val location: String
+        get() = _location
+
+    private var _locationOption by mutableStateOf("")
+    val locationOption: String
+        get() = _locationOption
+
+    private var _grade by mutableStateOf("")
+    val grade: String
+        get() = _grade
+
+    private var _gradeOption by mutableStateOf("")
+    val gradeOption: String
+        get() = _gradeOption
+
     fun onUpdateRange(start: Long, end: Long) {
         _start = start
         _end = end
+    }
+
+    fun onUpdateLocation(location: String) {
+        _location = location
+        _locationOption =
+            options.value?.locations?.find { it.name == location }?.options?.firstOrNull() ?: ""
+    }
+
+    fun onUpdateLocationOption(locationOption: String) {
+        _locationOption = locationOption
+    }
+
+    fun onUpdateGrade(grade: String) {
+        _grade = grade
+        _gradeOption =
+            options.value?.grades?.find { it.name == grade }?.options?.firstOrNull() ?: ""
+    }
+
+    fun onUpdateGradeOption(gradeOption: String) {
+        _gradeOption = gradeOption
+    }
+
+    private var _options = MutableLiveData<CourseOptionResponse>()
+    val options: LiveData<CourseOptionResponse>
+        get() = _options
+
+    fun loadOptions() {
+        val result = CourseOptionResponse(
+            locations = listOf(
+                GroupItem("서울", listOf("서울 전체")),
+                GroupItem("부산", listOf("부산 전체")),
+            ),
+            grades = listOf(
+                GroupItem("초등", listOf("초등 1학년", "초등 2학년", "초등 3학년")),
+                GroupItem("중등", listOf("중등 1학년", "중등 2학년", "중등 3학년")),
+                GroupItem("고등", listOf("고등 1학년", "고등 2학년", "고등 3학년")),
+            ),
+            keywords = listOf("키워드1", "키워드2", "키워드3", "키워드4")
+        )
+        _options.value = result
+        _location = result.locations.firstOrNull()?.name ?: _location
+        _locationOption = result.locations.firstOrNull()?.options?.firstOrNull() ?: ""
+        _grade = result.grades.firstOrNull()?.name ?: _grade
+        _gradeOption = result.grades.firstOrNull()?.options?.firstOrNull() ?: ""
     }
 }
