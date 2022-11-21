8c0f6b5e9cb82c2d2245735f43772c82e3507ad0
Lee Seung Yun <ileilliat@gmail.com>
2022-11-21 17:29:15 +0900

    Modify return value type at HomeViewModel

--- app/src/main/java/com/leeseungyun1020/learningtrip/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/viewmodel/HomeViewModel.kt
@@ -1,15 +1,20 @@
 package com.leeseungyun1020.learningtrip.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.leeseungyun1020.learningtrip.data.HomeRepository
+import com.leeseungyun1020.learningtrip.model.Banner
+import com.leeseungyun1020.learningtrip.model.Keyword
+import com.leeseungyun1020.learningtrip.model.SimpleCourse
+import com.leeseungyun1020.learningtrip.model.SimplePlace
 
 class HomeViewModel(private val repository: HomeRepository = HomeRepository()) : ViewModel() {
     init {
         repository.loadHomeContents()
     }
 
-    val recommendedKeywords = repository.recommendedKeywords
-    val mainBanners = repository.mainBanners
-    val recommendedPlaces = repository.recommendedPlaces
-    val recommendedCourses = repository.recommendedCourses
+    val recommendedKeywords: LiveData<List<Keyword>> = repository.recommendedKeywords
+    val mainBanners: LiveData<List<Banner>> = repository.mainBanners
+    val recommendedPlaces: LiveData<List<SimplePlace>> = repository.recommendedPlaces
+    val recommendedCourses: LiveData<List<SimpleCourse>> = repository.recommendedCourses
 }
