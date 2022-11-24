ccd32be72a4d64ffbd0d478fa3653491739de5d7
Lee Seung Yun <ileilliat@gmail.com>
2022-11-24 11:27:50 +0900

    Add title at CategoryScreen

--- app/src/main/java/com/leeseungyun1020/learningtrip/ui/category/CategoryScreen.kt
+++ app/src/main/java/com/leeseungyun1020/learningtrip/ui/category/CategoryScreen.kt
@@ -20,6 +20,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.leeseungyun1020.learningtrip.R
 import com.leeseungyun1020.learningtrip.ui.Screen
+import com.leeseungyun1020.learningtrip.ui.common.LearningTripScaffold
 import com.leeseungyun1020.learningtrip.viewmodel.CategoryViewModel
 
 @Composable
@@ -31,56 +32,60 @@ fun CategoryScreen(
     if (category == null)
         categoryViewModel.loadCategory()
 
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(10.dp),
-        modifier = Modifier
-            .padding(start = 16.dp, end = 16.dp),
-        contentPadding = PaddingValues(top = 16.dp)
+    LearningTripScaffold(
+        title = stringResource(id = R.string.nav_category),
     ) {
-        val categoryList = category ?: emptyList()
-        items(count = categoryList.size) {
-            var isOpen by remember { mutableStateOf(false) }
-            Column {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { isOpen = !isOpen }
-                ) {
-                    Text(
-                        text = categoryList[it].name,
-                        style = MaterialTheme.typography.bodyLarge,
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp),
+            contentPadding = PaddingValues(top = 16.dp)
+        ) {
+            val categoryList = category ?: emptyList()
+            items(count = categoryList.size) {
+                var isOpen by remember { mutableStateOf(false) }
+                Column {
+                    Box(
                         modifier = Modifier
-                            .padding(16.dp)
-                            .align(Alignment.Center)
-                    )
-                    if (isOpen) {
-                        Icon(
-                            imageVector = Icons.Default.ExpandLess,
-                            contentDescription = stringResource(id = R.string.action_expand_less),
-                            modifier = Modifier.align(Alignment.CenterEnd)
-                        )
-                    } else {
-                        Icon(
-                            imageVector = Icons.Default.ExpandMore,
-                            contentDescription = stringResource(id = R.string.action_expand_more),
-                            modifier = Modifier.align(Alignment.CenterEnd)
-                        )
-                    }
-                }
-                if (isOpen) {
-                    for (option in categoryList[it].options) {
-                        Box(modifier = Modifier
-                            .background(MaterialTheme.colorScheme.secondary)
                             .fillMaxWidth()
-                            .clickable { navController.navigate("${Screen.Search.root}/${categoryList[it].name} $option") }) {
-                            Text(
-                                text = option,
-                                style = MaterialTheme.typography.bodySmall,
-                                modifier = Modifier
-                                    .padding(16.dp)
-                                    .align(Alignment.Center),
-                                color = MaterialTheme.colorScheme.primary
+                            .clickable { isOpen = !isOpen }
+                    ) {
+                        Text(
+                            text = categoryList[it].name,
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .align(Alignment.Center)
+                        )
+                        if (isOpen) {
+                            Icon(
+                                imageVector = Icons.Default.ExpandLess,
+                                contentDescription = stringResource(id = R.string.action_expand_less),
+                                modifier = Modifier.align(Alignment.CenterEnd)
                             )
+                        } else {
+                            Icon(
+                                imageVector = Icons.Default.ExpandMore,
+                                contentDescription = stringResource(id = R.string.action_expand_more),
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            )
+                        }
+                    }
+                    if (isOpen) {
+                        for (option in categoryList[it].options) {
+                            Box(modifier = Modifier
+                                .background(MaterialTheme.colorScheme.secondary)
+                                .fillMaxWidth()
+                                .clickable { navController.navigate("${Screen.Search.root}/${categoryList[it].name} $option") }) {
+                                Text(
+                                    text = option,
+                                    style = MaterialTheme.typography.bodySmall,
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .align(Alignment.Center),
+                                    color = MaterialTheme.colorScheme.primary
+                                )
+                            }
                         }
                     }
                 }
